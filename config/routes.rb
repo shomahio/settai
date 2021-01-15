2894bc7933b33d5a504690a3b37be18d31529b63
shomahio <potter021401@gmail.com>
2021-01-15 14:32:38 +0900

    user,storeテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'informations/index'
+  devise_for :stores
+  devise_for :users
+  #root to: "informations#index"
 end
